53eee3a462028d5121832ea237441c7a1b5944b5
uldj-y <57079040+uldj-y@users.noreply.github.com>
2022-06-13 22:15:49 -0500

    Add files via upload

---
+++ Mi Oaxaca.php
@@ -0,0 +1,220 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<title>Mi Oaxaca</title>
+<style type="text/css">
+<!--
+body {
+	background-color: #E8E7E6;
+}
+#Layer1 {
+	position:absolute;
+	width:1360px;
+	height:110px;
+	z-index:1;
+	left: 2px;
+	top: 356px;
+	background-color: #E8DCD5;
+}
+.Estilo1 {color: #FFFFFF}
+#Layer2 {
+	position:absolute;
+	width:373px;
+	height:401px;
+	z-index:2;
+	left: 97px;
+	top: 867px;
+	background-color: #800014;
+}
+#Layer3 {
+	position:absolute;
+	width:373px;
+	height:401px;
+	z-index:3;
+	left: 820px;
+	top: 867px;
+	background-color: #800014;
+}
+.Estilo3 {font-weight: bold}
+#Layer4 {
+	position:absolute;
+	width:373px;
+	height:332px;
+	z-index:4;
+	left: 87px;
+	visibility: hidden;
+	top: 1334px;
+	background-color: #800014;
+}
+#Layer5 {
+	position:absolute;
+	width:373px;
+	height:401px;
+	z-index:5;
+	left: 97px;
+	top: 1360px;
+	background-color: #800014;
+}
+#Layer6 {
+	position:absolute;
+	width:373px;
+	height:401px;
+	z-index:6;
+	left: 820px;
+	top: 1360px;
+	background-color: #800014;
+}
+#Layer7 {
+	position:absolute;
+	width:373px;
+	height:401px;
+	z-index:7;
+	left: 97px;
+	background-color: #800014;
+	top: 1845px;
+}
+#Layer8 {
+	position:absolute;
+	width:373px;
+	height:401px;
+	z-index:8;
+	left: 820px;
+	top: 1840px;
+	background-color: #800014;
+}
+#Layer9 {
+	position:absolute;
+	width:1360px;
+	height:297px;
+	z-index:9;
+	left: 5px;
+	background-color: #986C35;
+}
+.Estilo4 {
+	color: #FFFFFF;
+	font-family: Arial, Helvetica, sans-serif;
+	font-size: 18px;
+}
+-->
+</style></head>
+
+<body>
+<div align="center">
+  <p><img src="Principal.png" width="460" height="400" alt="Prin"></p>
+  <div id="Layer1">
+    <p>&nbsp;</p>
+    <table width="1000" border="1" align="center">
+      <tr>
+        <th width="250" bordercolor="#986C35" bgcolor="#986C35" scope="col"><span class="Estilo1">Inicio</span></th>
+        <th width="250" bordercolor="#986C35" bgcolor="#986C35" scope="col"><span class="Estilo1">Donanos</span></th>
+        <th width="237" bordercolor="#986C35" bgcolor="#986C35" scope="col"><span class="Estilo1">Terminnos y condiciones </span></th>
+      </tr>
+    </table>
+  </div>
+  <p>&nbsp;  </p>
+</div>
+<div id="Layer2">
+  <div align="center" class="Estilo3">
+    <p>&nbsp;</p>
+    <p><img src="Logocon-removebg-preview.png" alt="Logo" width="120" height="100"></p>
+    <p class="Estilo1">San Pedro y San Pablo Teposcolula </p>
+    <p class="Estilo1"><a href="https://goo.gl/maps/V31XDjvRHwoFwo6S6"><img src="Teposcolula.jpg" alt="Tepos" width="298" height="169" border="0"></a></p>
+    <p class="Estilo1">&nbsp;</p>
+  </div>
+</div>
+<div id="Layer3">
+  <div align="center">
+    <p>&nbsp;</p>
+    <p><img src="Logocon-removebg-preview.png" alt="Logo2" width="120" height="100"></p>
+    <p class="Estilo1">Mazunte</p>
+    <p class="Estilo1"><a href="https://goo.gl/maps/7AmFEwfW3HReTLWd8"><img src="Mazunte.jpg" alt="Mazu" width="298" height="169" border="0"></a></p>
+  </div>
+</div>
+<p align="center"><img src="Pueblos-removebg-preview.png" alt="Pueblo" width="598" height="418"></p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<div id="Layer4"><img src="Logocon-removebg-preview.png" alt="Logo3" width="589" height="424"></div>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<div id="Layer5">
+  <div align="center">
+    <p>&nbsp;</p>
+    <p><img src="Logocon-removebg-preview.png" alt="Logo3" width="120" height="100"></p>
+    <p class="Estilo1">San Pablo Villa de Mitla</p>
+    <p class="Estilo1"><a href="https://goo.gl/maps/E7aL5H4FLHH3PCQD8"><img src="Mitla.jpeg" alt="Mitla" width="298" height="169" border="0"></a></p>
+  </div>
+</div>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<div id="Layer6">
+  <div align="center">
+    <p>&nbsp;</p>
+    <p><img src="Logocon-removebg-preview.png" alt="Logo4" width="120" height="100"></p>
+    <p class="Estilo1">Santa Catarina Juquila</p>
+    <p class="Estilo1"><a href="https://goo.gl/maps/kFfh56Ti7yRBqdww6"><img src="Juquila.jpg" alt="Juqui" width="298" height="169" border="0"></a></p>
+  </div>
+</div>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<div id="Layer7">
+  <div align="center">
+    <p>&nbsp;</p>
+    <p><img src="Logocon-removebg-preview.png" alt="Logo5" width="120" height="100"></p>
+    <h3 class="Estilo1" dir="ltr"><strong>Huautla de Jim&eacute;nez</strong></h3>
+    <p><a href="https://goo.gl/maps/mhay6swa5WfXKDTJ7"><img src="Huautla.jpg" alt="Huau" width="298" height="169" border="0"></a></p>
+  </div>
+</div>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<div id="Layer8">
+  <div align="center">
+    <p>&nbsp;</p>
+    <p><img src="Logocon-removebg-preview.png" alt="Logo6" width="120" height="100"></p>
+    <h3 class="Estilo1" dir="ltr"><strong>Capul&aacute;lpam de M&eacute;ndez</strong></h3>
+    <p><a href="https://goo.gl/maps/ATAiDH4oS6gD6Y7H6"><img src="Capulalpam.jpg" alt="Cupal" width="298" height="169" border="0"></a></p>
+  </div>
+</div>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p align="center"><img src="Secundario.png" alt="Secu" width="300" height="300"></p>
+<div id="Layer9">
+  <h2 align="center" class="Estilo4">Sobre la pagina de Pueblos M&aacute;gicos del Estado de Oaxaca</h2>
+    <h3 align="center" class="Estilo4">Queremos inspirarte a viajar, a ver lugares hermosos, a disfrutar las cosas buenas de M&eacute;xico y el mundo en especial los pueblos magicos que tiene nuestro estado para ofrecerte. Queremos que seas un viajero bien informado y que siempre consigas el mejor servicio al precio m&aacute;s conveniente.</h3>
+    <h3 align="center" class="Estilo4">&iexcl;Muchas Gracias por visitarnos! </h3>
+    <p align="center" class="Estilo4">Comunicate con nosotros:</p>
+    <p align="center" class="Estilo4"><a href="jonathanhuertaortega8@gmail.com"><img src="correo.png" alt="Gmail" width="52" height="52" hspace="20" border="0"></a><a href="9532110961"><img src="telefono.png" alt="Telf" width="52" height="52" hspace="18" border="0"></a></p>
+    <p align="center" class="Estilo4">Elaborador por:Jonathan Jes&uacute;s Huerta Ortega, Yuremi Yudibeth Lop&eacute;z de Jes&uacute;s y Abner Matinez Ortiz </p>
+</div>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+</body>
+</html>
